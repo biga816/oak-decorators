b5f66c4f4ddb891f1d9ce53aaebf7f014ffd5434
akihiro.tanaka <big.a.0816@gmail.com>
2022-01-14 01:00:39 +0900

    remove class validator

--- src/decorators/route-params.decorator.ts
+++ src/decorators/route-params.decorator.ts
@@ -1,14 +1,19 @@
-import { Reflect, isNotEmpty, isString } from "../deps.ts";
+import { Reflect } from "../deps.ts";
 import { RouteParamtypes } from "../enums/mod.ts";
 import { ROUTE_ARGS_METADATA } from "../const.ts";
 import { RouteArgsMetadata, ParamData } from "../interfaces/mod.ts";
+
+const isUndefined = (obj: any): obj is undefined => typeof obj === "undefined";
+const isString = (fn: any): fn is string => typeof fn === "string";
+const isNil = (obj: any): obj is null | undefined =>
+  isUndefined(obj) || obj === null;
 
 function createPipesRouteParamDecorator(paramtype: RouteParamtypes) {
   return (data?: ParamData): ParameterDecorator =>
     (target, key, index) => {
       const args: RouteArgsMetadata[] =
         Reflect.getMetadata(ROUTE_ARGS_METADATA, target, key) || [];
-      const hasParamData = isNotEmpty(data) || isString(data);
+      const hasParamData = isNil(data) || isString(data);
       const paramData = hasParamData ? data : undefined;
 
       args.push({
--- src/deps.ts
+++ src/deps.ts
@@ -3,17 +3,12 @@ import {
   Router,
   RouterContext,
   helpers,
-} from "https://deno.land/x/oak@v10.0.0/mod.ts";
+} from "https://deno.land/x/oak@v10.1.0/mod.ts";
 
 export { Application, Router, helpers };
 export type { RouterContext };
 
 export { Reflect } from "https://deno.land/x/reflect_metadata@v0.1.12-2/mod.ts";
-
-export {
-  isNotEmpty,
-  isString,
-} from "https://deno.land/x/deno_class_validator@v1.0.0/mod.ts";
 
 export {
   bootstrap,
