6350329ba45191465286001c0fe24ad4c2c080e4
Nicola Dal Pont <ndalpont@protonmail.com>
2023-12-20 10:44:47 +1030

    Simplified Middleware Decorators registration Improved documentation

--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,3 @@
 export { Injectable } from './deps.ts';
 export * from './decorators/mod.ts';
 export * from './utils/router.util.ts';
-export { MIDDLEWARE_METADATA } from './const.ts';
--- src/utils/router.util.ts
+++ src/utils/router.util.ts
@@ -1,7 +1,8 @@
-import { Reflect, Router, bootstrap } from "../deps.ts";
+import { Reflect, Router, bootstrap } from '../deps.ts';
 
-import { MODULE_METADATA } from "../const.ts";
-import { CreateRouterOption } from "../interfaces/mod.ts";
+import { MIDDLEWARE_METADATA, MODULE_METADATA } from '../const.ts';
+import { CreateRouterOption } from '../interfaces/mod.ts';
+import { RouterContext, Next } from 'oak';
 
 const createRouter = (
   { controllers, providers, routePrefix }: CreateRouterOption,
@@ -9,10 +10,10 @@ const createRouter = (
   router = new Router()
 ) => {
   controllers.forEach((Controller) => {
-    Reflect.defineMetadata("design:paramtypes", providers, Controller);
+    Reflect.defineMetadata('design:paramtypes', providers, Controller);
     const controller = bootstrap<any>(Controller);
     const prefixFull = prefix
-      ? prefix + (routePrefix ? `/${routePrefix}` : "")
+      ? prefix + (routePrefix ? `/${routePrefix}` : '')
       : routePrefix;
     controller.init(prefixFull);
     const path = controller.path;
@@ -40,4 +41,24 @@ const getRouter = (module: any, prefix?: string, router?: Router) => {
 export const assignModule = (module: any) => {
   const router = getRouter(module);
   return router.routes();
+};
+
+/**
+ * Registers a decorator that can be added to a controller's
+ * method. The handler will be called at runtime when the
+ * endpoint method is invoked with the Context and Next parameters.
+ *
+ * @param target decorator's target
+ * @param methodName decorator's method name
+ * @param handler decorator's handler
+ */
+export const registerMiddlewareMethodDecorator = (
+  target: any,
+  methodName: Function,
+  handler: (ctx: RouterContext, next: Next) => void
+) => {
+  const middleware =
+    Reflect.getMetadata(MIDDLEWARE_METADATA, target, methodName) || [];
+  middleware.push(handler);
+  Reflect.defineMetadata(MIDDLEWARE_METADATA, middleware, target, methodName);
 };
